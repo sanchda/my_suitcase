8241ad8aba243c82d0d90999dbf8f887547e0f7f
sanchda <838104+sanchda@users.noreply.github.com>
2025-06-18 14:37:18 +0000

    Moar

---
+++ personalize/get_bcc/get_deps.ubuntu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt install -y zip bison build-essential cmake flex git libedit-dev \
+  libllvm14 llvm-14-dev libclang-14-dev python3 zlib1g-dev libelf-dev libfl-dev python3-setuptools \
+  liblzma-dev libdebuginfod-dev arping netperf iperf
